843d1b7d46858243d55e154bacf03006a002c139
Gabriele Sotto <gabriele.sotto@nttdata.com>
2019-02-11 16:45:16 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='mltools',
       version='0.1',
       description='A custom collection of ML methods and utilities',
-      url='https://github.com/Tostox/python_packages/mltools',
+      url='https://github.com/Tostox/python_packages',
       author='Gabriele Sotto',
       packages=['mltools'],
       zip_safe=False)
